3643065fbae930d7f47d112d557e4484bad28eb8
Gabriel Henrique <ghlinsramos@gmail.com>
2025-07-22 20:47:38 -0300

    Atividade Pratica 1

---
+++ atividade4.py
@@ -0,0 +1,11 @@
+nome = "Cadeira Infantil"
+preco = 12.4
+quantidade = 3
+
+total = preco*quantidade
+
+#Obs: usei vários prints apenas por questão de organização. Mas sei que poderia ser um unico print e usar o quebrar linha com \n
+print("-------------------------------------------------------------------------------------")
+print(f"Nome do produto: {nome} | Preço unitário: R$ {preco} | Quantidade: {quantidade}")
+print("-------------------------------------------------------------------------------------")
+print(f"Total: {total}")
